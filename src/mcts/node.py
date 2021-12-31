46877e1a0154c10d1feec53c9b4aa1948b87826d
koume80703 <kou.taka1147@gmail.com>
2021-12-31 14:12:37 +0900

    othello.py separated to 3 files, and removed IState.py

--- src/mcts/node.py
+++ src/mcts/node.py
@@ -1,45 +1,52 @@
 from __future__ import annotations
+
+from typing import List
+
 from mcts.istate import IState
 from mcts.util.argmax import argmax
 from mcts.util.ucb1 import ucb1
 
 import copy
 
+
 class Node:
     def __init__(self, state: IState, expand_base: int = 20) -> None:
         self.state: IState = copy.deepcopy(state)
-        self.w: int = 0
+        self.w: float = 0
         self.n: int = 0
         self.expand_base: int = expand_base
-        self.children: Optional[List[Node]] = None
+        self.children: List[Node] = []
 
     def evaluate(self) -> float:
         """現在のノードの評価値を計算して更新する。"""
-        
+
         if self.state.is_done():
             value = -1 if self.state.is_lose() else 0
             self.w += value
             self.n += 1
             return value
 
-        if self.children is None:            
-            v = Node.playout(copy.deepcopy(self.state))            
-            self.w += v
+        if self.children == []:
+            value = Node.playout(copy.deepcopy(self.state))
+            self.w += value
             self.n += 1
 
             if self.n == self.expand_base:
                 self.expand()
-            return v
+            return value
         else:
-            v = self.next_child_based_ucb().evaluate()
-            self.w += v
+            value = self.next_child_based_ucb().evaluate()
+            self.w += value
             self.n += 1
-            return v
+            return value
 
     def expand(self) -> None:
         if self.state.legal_actions() == []:
             return
-        self.children = [Node(self.state.next(action), self.expand_base) for action in self.state.legal_actions()]        
+        self.children = [
+            Node(self.state.next(action), self.expand_base)
+            for action in self.state.legal_actions()
+        ]
 
     def next_child_based_ucb(self) -> Node:
         for child in self.children:
@@ -48,7 +55,7 @@ class Node:
 
         sn = sum([child.n for child in self.children])
         ucb1_values = [ucb1(sn, child.n, child.w) for child in self.children]
-        
+
         return self.children[argmax(ucb1_values)]
 
     @classmethod
--- src/state.py
+++ src/state.py
@@ -1,43 +1,52 @@
 from __future__ import annotations
-from random import choice
-from mcts.istate import IState
-from typing import List, Optional
-import copy
 
-class State(IState):
+from game import Game
+
+import copy
+from random import choice
+from typing import List, Tuple, Optional
+
+
+class State:
     def __init__(self, game: Game):
         self.game = copy.deepcopy(game)
 
-    def next(self, action: (int, int)) -> State:
+    def next(self, action: Tuple[int, int]) -> State:
         n_state = State(self.game)
         n_state.game.set_stone(*action)
         return n_state
-    
+
     def pass_moving(self) -> State:
         n_state = State(self.game)
         n_state.game.pass_moving()
         return n_state
-    
-    def legal_actions(self) -> List[(int, int)]:
+
+    def legal_actions(self) -> List[Tuple[int, int]]:
         return self.game.list_placable_stone()
-    
-    def random_action(self) -> (int,int):
+
+    def random_action(self) -> Tuple[int, int]:
         return choice(self.legal_actions())
 
-    def winner(self) -> int:
+    def winner(self) -> Optional[int]:
         return self.game.winner
 
     def is_win(self) -> bool:
-        return True if self.game.START_PLAYER == self.winner() else False
+        winner = self.winner()
+        if winner is None:
+            return False
+        return True if self.game.START_PLAYER == winner else False
 
     def is_lose(self) -> bool:
-        return True if self.game.START_PLAYER == -self.winner() else False
+        winner = self.winner()
+        if winner is None:
+            return False
+        return True if self.game.START_PLAYER == -winner else False
 
     def is_draw(self) -> bool:
         return True if self.winner() == self.game.DRAW else False
 
     def is_done(self) -> bool:
         return self.game.is_finished()
-    
+
     def is_first_player(self) -> bool:
         return self.game.START_PLAYER == self.game.player
